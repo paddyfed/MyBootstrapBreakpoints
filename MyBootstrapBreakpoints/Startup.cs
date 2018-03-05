ce20ca59b5f6a2cf32e2ef5dcc67510136a9a719
Paddy O Hara <paddyfedoo@hotmail.com>
2018-03-05 21:50:04 +0000

    Required https and redirected from http

--- MyBootstrapBreakpoints/Startup.cs
+++ MyBootstrapBreakpoints/Startup.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,11 +25,20 @@ namespace MyBootstrapBreakpoints
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new RequireHttpsAttribute());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            var options = new RewriteOptions().AddRedirectToHttps(StatusCodes.Status301MovedPermanently, 63423);
+
+            app.UseRewriter(options);
+
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
